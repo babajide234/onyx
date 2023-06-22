d8a174903180f5389cbd9b42fbe66c1206d4173c
First Medtrade Africa <info@firstmedtradeafrica.com>
2023-06-22 06:14:11 +0100

    sdsf

--- src/components/pages/about/Header.jsx
+++ src/components/pages/about/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import Container from "../../common/Container"
 import { HeaderOne } from "../../common/Typgraphy"
 import IMG1 from '../../../assets/aboutimage1.png'
@@ -5,8 +6,15 @@ import IMG2 from '../../../assets/aboutimage2.png'
 import IMG3 from '../../../assets/aboutimage3.png'
 import IMG4 from '../../../assets/aboutimage4.png'
 import { motion } from "framer-motion"
+import { useInView } from "react-intersection-observer";
 
 const Header = () => {
+
+    const [ref1, inView1] = useInView();
+    const [ref2, inView2] = useInView();
+    const [ref3, inView3] = useInView();
+    const [ref4, inView4] = useInView();
+
   return (
     <header className="md:min-h-screen">
     <Container>
@@ -20,39 +28,44 @@ const Header = () => {
             </div>
             <div className=" w-full md:w-1/2 py-10 flex justify-end relative">
                 <div className=" relative flex gap-5">
-                    <motion.img
-                        initial={{  y: 500 }}
-                        animate={{  y: 0 }}
-                        exit={{ y:500}}
-                        transition={{ duration: 1 }}
-                        src={IMG1}
-                        alt=""
-                        className=""
-                    />
-                    <motion.img
-                        initial={{  y: -500 }}
-                        animate={{  y: 50 }}
-                        transition={{ duration: 1 }}
-                        src={IMG2}
-                        alt=""
-                        className=""
-                    />
-                    <motion.img
-                        initial={{  y: 500 }}
-                        animate={{  y: 0 }}
-                        transition={{ duration: 1 }}
-                        src={IMG3}
-                        alt=""
-                        className=""
-                    />
-                    <motion.img
-                        initial={{  y: -500 }}
-                        animate={{  y: 50 }}
-                        transition={{ duration: 1 }}
-                        src={IMG4}
-                        alt=""
-                        className=""
-                    />
+                <motion.img
+                    ref={ref1}
+                    initial={{ y: 500 }}
+                    animate={inView1 ? { y: 0 } : { y: 500 }}
+                    exit={{ y: 500 }}
+                    transition={{ duration: 1 }}
+                    src={IMG1}
+                    alt=""
+                    className=""
+                />
+                <motion.img
+                ref={ref2}
+                initial={{ y: -500 }}
+                animate={inView2 ? { y: 50 } : { y: -500 }}
+                transition={{ duration: 1 }}
+                src={IMG2}
+                alt=""
+                className=""
+                />
+                <motion.img
+                ref={ref3}
+                initial={{ y: 500 }}
+                animate={inView3 ? { y: 0 } : { y: 500 }}
+                exit={{ y: 500 }}
+                transition={{ duration: 1 }}
+                src={IMG3}
+                alt=""
+                className=""
+                />
+                <motion.img
+                ref={ref4}
+                initial={{ y: -500 }}
+                animate={inView4 ? { y: 50 } : { y: -500 }}
+                transition={{ duration: 1 }}
+                src={IMG4}
+                alt=""
+                className=""
+                />
                 </div>
             </div>
         </div>
